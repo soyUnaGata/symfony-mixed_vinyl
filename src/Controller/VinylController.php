d389438fb6cc228806a376cf31a33e74b685a278
soyUnaGata <fallen.snitch@gmail.com>
2024-09-16 12:45:12 +0300

    set up twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,21 @@
 
 namespace App\Controller;
 
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 class VinylController
 {
+ #[Route('/', name: 'vinyl-home')]
+    public function homepage(): Response
+    {
+        return new Response('Here');
+    }
 
+    #[Route('/browse/{slug}', name: 'vinyl')]
+    public function browse(string $slug): Response {
+
+     return new Response('Genre:'. $slug);
+    }
 }
